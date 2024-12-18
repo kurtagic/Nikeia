0abe6cfc5401d0396a1f434b885c5074418387ca
teo kurtagic <teo@sl-king.com>
2024-12-18 20:33:32 +0100

    fakjk

--- Instructions.h
+++ Instructions.h
@@ -1,18 +1,5 @@
 #pragma once
 
-/*
-// NIKEIA INSTRUCTIONS
-// PUSH - push to stack
-// POP - pop from stack
-// ADD - pop 2 numbers from stack, push the sum
-// SUB - pop 2 numbers from stack, push the difference
-// JUMP_EQL_0 - OPCODE LABEL: jump to label if top of stack is <= 0
-// JUMP_GRT_0 - OPCODE LABEL: jump to label if top of stack is > 0
-// PRINT OPCODE STRING_LITERAL: prints the string_literal to the terminal
-// READ OPCODE: read number from I/O and push to stack
-// HALT stop program
-*/
-
 #define PUSH "PUSH"
 #define POP "POP"
 #define ADD "ADD"
